59dcf915fd66b921247fc120dfa476f295dcd933
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-03-26 21:54:25 -0700

    Added column

--- index.php
+++ index.php
@@ -4,11 +4,17 @@
 <!doctype html>
 <html>
 	<body>
-	
+
 		<div class="jumbotron">
 			<div class="container">
 				<h1>Blaze Game Studios</h1>
 			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">h1</div>
+			<div class="col-md-4">h1</div>
+			<div class="col-md-4">h1</div>
 		</div>
 	</body>
 </html>
