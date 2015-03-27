4fbb6df005de5e766a38940973528b3d38ebbe94
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-03-26 21:52:45 -0700

    Float right navbar

--- include/header.php
+++ include/header.php
@@ -20,7 +20,7 @@
     			</div>
     			
     			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      				<ul class="nav navbar-nav">
+      				<ul class="nav navbar-nav navbar-right">
         				<li><a href="index.php">Home</a></li>
         				<li><a href="games.php">Games</a></li>
         				<li><a href="contact.php">Contact</a></li>
