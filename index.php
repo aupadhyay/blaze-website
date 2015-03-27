f23820bb921d4d711a1c71cef2c8652c8070cc6a
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-03-26 21:56:05 -0700

    Added app content

--- index.php
+++ index.php
@@ -10,11 +10,20 @@
 				<h1>Blaze Game Studios</h1>
 			</div>
 		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h3>Soccer Burst</h3>
+				</div>
 
-		<div class="row">
-			<div class="col-md-4">h1</div>
-			<div class="col-md-4">h1</div>
-			<div class="col-md-4">h1</div>
+				<div class="col-md-4">
+					<h3>Grave Buster</h3>
+				</div>
+				
+				<div class="col-md-4">
+					<h3>Commander Comet</h3>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
