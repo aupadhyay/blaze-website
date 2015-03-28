af1f8e26920031c9c7fef7a0a0b377684f704fa3
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-03-27 20:06:48 -0700

    Added Play Store links

--- index.php
+++ index.php
@@ -8,19 +8,31 @@
 		<div class="jumbotron">
 			<div class="container">
 				<h1>Blaze Game Studios</h1>
+				
 			</div>
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-sm-4">
 					<h3>Soccer Burst</h3>
+					
+					<br>
+					
+					<a href="https://play.google.com/store/apps/details?id=com.blazegamestudios.soccerburst">
+  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+  					</a>
+				
 				</div>
 
-				<div class="col-md-4">
+				<div class="col-sm-4">
 					<h3>Grave Buster</h3>
+
+					<a href="https://play.google.com/store/apps/details?id=com.blazegamestudios.gravebuster">
+  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+  					</a>
 				</div>
 				
-				<div class="col-md-4">
+				<div class="col-sm-4">
 					<h3>Commander Comet</h3>
 				</div>
 			</div>
