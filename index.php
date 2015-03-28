3936dbe9318be1cfda5db502d5fe9fa266855602
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-03-27 20:39:19 -0700

    Playing with size

--- index.php
+++ index.php
@@ -17,13 +17,13 @@
 					<h3>Soccer Burst</h3>
 					
 					<a href="https://itunes.apple.com/TR/app/id908532523">
-  						<img alt="Get it on the App Store" src="img/app_store_apple.svg" />
+  						<img alt="Get it on the App Store" src="img/app_store_apple.svg" width="200" />
   					</a>
 					
 					<br>
 					
 					<a href="https://play.google.com/store/apps/details?id=com.blazegamestudios.soccerburst">
-  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" width="200" />
   					</a>
 				
 				</div>
@@ -32,13 +32,13 @@
 					<h3>Grave Buster</h3>
 					
 					<a href="https://itunes.apple.com/us/app/grave-buster/id908537151">
-  						<img alt="Get it on the App Store" src="img/app_store_apple.svg" width="300" />
+  						<img alt="Get it on the App Store" src="img/app_store_apple.svg" width="200" />
   					</a>
 					
 					<br>
 					
 					<a href="https://play.google.com/store/apps/details?id=com.blazegamestudios.gravebuster">
-  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" width="300" />
+  						<img alt="Get it on Google Play" src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" width="200" />
   					</a>
 				</div>
 				
