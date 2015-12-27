ada88bb6e18dea354740f09634e3bb26a72ed5ca
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-12-27 02:33:29 +0000

    adding sketchit folder

---
+++ sketchit/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]){
+	Mat image;
+	cout<<"usage: "<< argv[1] <<" input\n";
+	cout << "Test12";
+	cin.get();
+	return 0;
+}
